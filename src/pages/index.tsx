6e093803f57b11d9a77a9e6c3327d50b5de8fa19
ryohei-miki <miki@n2i.jp>
2022-04-19 09:08:15 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import { memo, useState } from "react";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <div className={styles.container}>ここにコンテンツが入る</div>;
+};
+
+export default Home;
